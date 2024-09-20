4f19ba0b4a90c5da31c58bbbe67ca295bd2aed9d
Jefferson Santos <jeff-ds@outlook.com>
2024-09-20 15:33:13 -0300

    initial commit

---
+++ src/main/java/com/jeffersonsantos/agendamento_de_veiculos/AgendamentoDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.jeffersonsantos.agendamento_de_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffersonsantos/agendamento_de_veiculos/AgendamentoDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonsantos.agendamento_de_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
